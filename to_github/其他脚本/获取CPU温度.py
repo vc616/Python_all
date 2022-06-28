8e6714e5b1ae763e236f667945cc0cc98410893c
vc <vc616@qq.com>
2022-06-28 11:35:49 +0800

    1

---
+++ to_github/其他脚本/获取CPU温度.py
@@ -0,0 +1,7 @@
+from time import sleep
+from pyspectator.processor import Cpu
+cpu = Cpu(monitoring_latency=1)
+with cpu:
+    while True:
+        print(f'Temp: {cpu.temperature} °C')
+        sleep(2)

49f1776429d4ab461e7a08737ceba98ffa043827
vc <vc616@qq.com>
2022-06-29 09:58:23 +0800

    1

--- to_github/mysql/读取温度.py
+++ to_github/mysql/读取温度.py
@@ -7,28 +7,23 @@ db = pymysql.connect(host= '111.207.218.252', port= 8016, user= 'root', password
 cursor = db.cursor()
 
 # SQL 查询语句
-sql = """SELECT1 * FROM test.温度 温 where `time` > (NOW()- INTERVAL 1 HOUR)"""
-for i in range(6):
-   try:
-      s = 0
-      # 执行SQL语句
-      if i == 3:
-         sql = """SELECT * FROM test.温度 温 where `time` > (NOW()- INTERVAL 1 HOUR)"""
+sql = """SELECT * FROM test.温度 温 where `time` > (NOW()- INTERVAL 1 HOUR )"""
 
-      cursor.execute(sql)
-      # 获取所有记录列表      
-      results = cursor.fetchall()
-      for row in results:
-         time = row[0]
-         t = row[1]
-         h = row[2] 
-         # 打印结果
-         print( "时间=%s,湿度=%s,温度=%s" % (time, h, t ))
-         s = s +1
-      break
-   except:
-      print("Error: unable to fetch data")
-   print(i)
+try:
+   s = 0
+   # 执行SQL语句
+   cursor.execute(sql)
+   # 获取所有记录列表      
+   results = cursor.fetchall()
+   for row in results:
+      time = row[0]
+      t = row[1]
+      h = row[2] 
+      # 打印结果
+      print( "时间=%s,湿度=%s,温度=%s" % (time, h, t ))
+      s = s +1
+except:
+   print("Error: unable to fetch data")
 # 关闭数据库连接
 db.close()
 print(s)
